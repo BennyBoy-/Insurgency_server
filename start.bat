7a85030ab24f69adc6e99a983d1e585b86699805
BennyBoy- <rouleaub@gmail.com>
2022-12-28 21:21:06 +0100

    Server files

---
+++ start.bat
@@ -0,0 +1,1 @@
+InsurgencyServer.exe Bab?Scenario=Scenario_Bab_Checkpoint_Insurgents?MaxPlayers=10 -Port=27102 -QueryPort=27131 -log -hostname="France Strong" -GameStatsToken=XXX -MapCycle=MapCycles -mutators=SoldierOfFortune,Strapped
---
+++ update.bat
@@ -0,0 +1,3 @@
+@echo off
+title update.bat
+E:\Steamcmd\steamcmd.exe +runscript E:\Steamcmd\Sandstorm\sand_ds.txt
